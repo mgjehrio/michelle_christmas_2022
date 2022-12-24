f4ab46d19349a0fae36ef0963bde337853c74d69
mgjehrio <mgjehrio@buffalo.edu>
2022-12-24 17:34:44 -0500

    change directory style for github

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
 import streamlit as st
 
-st.video("C:\\Users\\mgjeh\Downloads\\Cameo_by_Robby_Takac(Goo Goo Dolls)_via_cameo.mp4", format="video/mp4", start_time=0)
+st.video("Cameo_by_Robby_Takac(Goo Goo Dolls)_via_cameo.mp4", format="video/mp4", start_time=0)
 
